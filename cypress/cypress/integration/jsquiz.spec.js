36d2f4a5f8b00e1c482371e583b433d28d9e89ab
Jamie Livingstone <jamie.livingstone@outlook.com>
2017-10-30 00:12:34 +0000

    Update meta tags for SEO

--- cypress/cypress/integration/jsquiz.spec.js
+++ cypress/cypress/integration/jsquiz.spec.js
@@ -4,4 +4,16 @@ describe('TheJSQuiz integration tests', () => {
   it('visits website without error', () => {
     cy.visit(websiteUrl);
   });
+
+  it('should register a service worker', () => {
+
+  });
+
+  it('should work offline', () => {
+
+  });
+
+  it('should return questions in correct format', () => {
+    
+  });
 });
